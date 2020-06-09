484f0eeed904393b2dc32c5ec8937287c2293132
sungbin@nate.com <sungbin@nate.com>
2020-06-09 16:23:50 +0900

    조회수

--- src/com/cos/blog/action/board/BoardDetailAction.java
+++ src/com/cos/blog/action/board/BoardDetailAction.java
@@ -9,7 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.cos.blog.action.Action;
 import com.cos.blog.dto.DetailResponseDto;
+import com.cos.blog.model.Board;
 import com.cos.blog.repository.BoardRepository;
+import com.cos.blog.util.HtmlParser;
 import com.cos.blog.util.Script;
 
 public class BoardDetailAction implements Action {
@@ -26,10 +28,23 @@ public class BoardDetailAction implements Action {
 		int id = Integer.parseInt(request.getParameter("id"));
 		BoardRepository boardRepository = 
 				BoardRepository.getInstance();
+		
+		//조회수 증가가 상세보기가 되기 전에 실행되는 것이 좋음
+		int result = boardRepository.updateReadCount(id);
+		
+		if(result != 1) {
+			Script.back("서버 오류!", response);
+			return;
+		}
 		DetailResponseDto dto = 
 				boardRepository.findById(id);
 		
 		if(dto != null) {
+			// 유튜브 파싱하기
+			String content = dto.getBoard().getContent();
+			content = HtmlParser.getContentYoutube(content);
+			dto.getBoard().setContent(content);
+			
 			request.setAttribute("dto", dto);
 			RequestDispatcher dis = 
 					request.getRequestDispatcher("board/detail.jsp");
